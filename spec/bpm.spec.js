5ad0783e82f908ad27fc962c23b0a1dc40f9aca1
Mariano Guerra <luismarianoguerra@gmail.com>
2014-02-06 14:27:32 +0100

    parse basic elements and test

--- spec/bpm.spec.js
+++ spec/bpm.spec.js
@@ -1,5 +1,7 @@
 /*global define, describe, it, expect*/
-define(['src/bpm', 'text!test-data/rfp.bpmn'], function (bpm, rfpXml) {
+define(['src/bpm', 'text!test-data/rfp.bpmn', 'text!test-data/recruit.bpmn',
+        'text!test-data/evaluation.bpmn'],
+       function (bpm, rfpXml, recruitXml, evaluationXml) {
     'use strict';
 
     describe('bpmjs', function () {
@@ -8,5 +10,43 @@ define(['src/bpm', 'text!test-data/rfp.bpmn'], function (bpm, rfpXml) {
             expect(typeof bpm.parseXml).toEqual("function");
         });
 
+        it('should parse rfp (bonita) xml', function () {
+            var model = bpm.parseXml(rfpXml);
+
+            expect(model).toBeDefined();
+            expect(model.connections.length).toBe(16);
+            expect(model.shapes.length).toBe(17);
+            expect(model.byType.userTask.length).toBe(3);
+            expect(model.byType.startEvent.length).toBe(1);
+            expect(model.byType.endEvent.length).toBe(2);
+            expect(model.byType.serviceTask.length).toBe(4);
+            expect(model.byType.exclusiveGateway.length).toBe(5);
+        });
+
+        it('should parse recruit (bonita) xml', function () {
+            var model = bpm.parseXml(recruitXml);
+
+            expect(model).toBeDefined();
+            expect(model.connections.length).toBe(37);
+            expect(model.shapes.length).toBe(39);
+            expect(model.byType.startEvent.length).toBe(3);
+            expect(model.byType.endEvent.length).toBe(4);
+            expect(model.byType.userTask.length).toBe(8);
+            expect(model.byType.serviceTask.length).toBe(6);
+            expect(model.byType.exclusiveGateway.length).toBe(5);
+            expect(model.byType.parallelGateway.length).toBe(4);
+        });
+
+        it('should parse evaluation (jboss) xml', function () {
+            var model = bpm.parseXml(evaluationXml);
+
+            expect(model).toBeDefined();
+            expect(model.connections.length).toBe(7);
+            expect(model.byType.startEvent.length).toBe(1);
+            expect(model.byType.endEvent.length).toBe(1);
+            expect(model.byType.userTask.length).toBe(3);
+            expect(model.byType.parallelGateway.length).toBe(2);
+            expect(model.shapes.length).toBe(7);
+        });
     });
 });
--- src/bpm.js
+++ src/bpm.js
@@ -1,8 +1,147 @@
 /*global define*/
 define(['jquery'], function ($) {
     "use strict";
+    var DI_NS = "http://www.omg.org/spec/BPMN/20100524/DI",
+        DC_NS = "http://www.omg.org/spec/DD/20100524/DC",
+        MODEL_NS = "http://www.omg.org/spec/BPMN/20100524/MODEL",
+
+        SHAPE = "BPMNShape",
+        BOUNDS = "Bounds",
+        LANE_SET = "laneSet",
+        LANE = "lane",
+        CONN_TYPES = ["sequenceFlow"],
+        MODELS = {
+            "exclusiveGateway": ["id", "name", "default", "gatewayDirection"],
+            "inclusiveGateway": ["id", "name", "default", "gatewayDirection"],
+            "parallelGateway": ["id", "name", "gatewayDirection"],
+            "eventBasedGateway": ["id", "name", "gatewayDirection"],
+
+            "sequenceFlow": ["id", "name", "sourceRef", "targetRef",
+                "isImmediate"],
+
+            "startEvent": ["id", "name"],
+            "endEvent": ["id", "name"],
+            "task": ["id", "name"],
+            "userTask": ["id", "name"],
+            "intermediateCatchEvent": ["id", "name"],
+            "intermediateThrowEvent": ["id", "name"],
+            "scriptTask": ["id", "name"],
+            "businessRuleTask": ["id", "name"],
+            "manualTask": ["id", "name"],
+            "sendTask": ["id", "name"],
+            "receiveTask": ["id", "name"],
+            "serviceTask": ["id", "name"],
+            "subProcess": ["id", "name"],
+            "adHocSubProcess": ["id", "name"],
+            "callActivity": ["id", "name"]
+        };
+
+    function xmlToObject(type, fields) {
+        return function (node) {
+            var i, field,
+                len = fields.length,
+                result = {nodeType: type};
+
+            for (i = 0; i < len; i += 1) {
+                field = fields[i];
+                result[field] = node.getAttribute(field) || "";
+            }
+
+            return result;
+        };
+    }
+
+    function attrToFloatOr(node, name, defaultValue) {
+        var valFloat, val = node.getAttribute(name);
+
+        if (typeof val === "string") {
+            valFloat = parseFloat(val);
+
+            // isNaN
+            if (valFloat !== valFloat) {
+                return defaultValue;
+            } else {
+                return valFloat;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+    function xmlToShape(node) {
+        var bounds,
+            result = {},
+            boundss = node.getElementsByTagNameNS(DC_NS, BOUNDS);
+
+        if (boundss.length > 0) {
+            bounds = boundss[0];
+
+            result.x = attrToFloatOr(bounds, "x", 0.0);
+            result.y = attrToFloatOr(bounds, "y", 0.0);
+            result.width = attrToFloatOr(bounds, "width", 0.0);
+            result.height = attrToFloatOr(bounds, "height", 0.0);
+            result.hasBounds = true;
+        } else {
+            result.x = 0.0;
+            result.y = 0.0;
+            result.width = 0.0;
+            result.height = 0.0;
+            result.hasBounds = false;
+        }
+
+        result.id = node.getAttribute("id");
+        result.bpmnElement = node.getAttribute("bpmnElement");
+        result.isHorizontal = !!node.getAttribute("isHorizontal");
+
+        return result;
+    }
+
+    function map(items, fun) {
+        var i, len = items.length,
+            result = new Array(len);
+
+        for (i = 0; i < len; i += 1) {
+            result[i] = fun(items[i]);
+        }
+
+        return result;
+    }
 
     function parseXml(xml) {
+        var seqFlows, shapes, type, node, nodes, i, len,
+            $xml = $.parseXML(xml),
+            doc = $xml.documentElement,
+            connections = [],
+            byId = {},
+            byType = {};
+
+        for (type in MODELS) {
+            nodes = map(doc.getElementsByTagNameNS(MODEL_NS, type),
+                           xmlToObject(type, MODELS[type]));
+
+            for (i = 0, len = nodes.length; i < len; i += 1) {
+                node = nodes[i];
+                byId[node.id] = node;
+
+                if (CONN_TYPES.indexOf(node.nodeType) !== -1) {
+                    connections.push(node);
+                }
+            }
+
+            byType[type] = nodes;
+        }
+
+        shapes = map(doc.getElementsByTagNameNS(DI_NS, SHAPE), xmlToShape);
+        shapes.forEach(function (shape) {
+            shape.model = byId[shape.bpmnElement] || null;
+        });
+
+        return {
+            byId: byId,
+            byType: byType,
+            shapes: shapes,
+            connections: connections
+        };
     }
 
     return {parseXml: parseXml};
